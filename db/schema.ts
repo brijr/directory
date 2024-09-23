c7bf85c0ff969875842ea0941ba40ff30c6b760a
Bridger Tower <bridgertower@gmail.com>
2024-09-23 16:25:30 -0600

    update schema to match current

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,21 @@
 import { sql } from "drizzle-orm";
-import { text, sqliteTable } from "drizzle-orm/sqlite-core";
+import { integer, text, sqliteTable } from "drizzle-orm/sqlite-core";
 
-export const fooTable = sqliteTable("foo", {
-  bar: text("bar").notNull().default("Hey!"),
+export const categories = sqliteTable("categories", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name").unique().notNull(),
+  slug: text("slug").unique().notNull(),
+});
+
+export const bookmarks = sqliteTable("bookmarks", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  title: text("title"),
+  description: text("description"),
+  url: text("url").unique().notNull(),
+  ogImage: text("og_image"),
+  summary: text("summary"),
+  screenshotName: text("screenshot_name"),
+  screenshotUrl: text("screenshot_url"),
+  slug: text("slug"),
+  categoryId: integer("category_id").references(() => categories.id),
 });
