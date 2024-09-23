ec1375d7e102a611fcccf0bbb25e39ccbe14dd6b
Bridger Tower <bridgertower@gmail.com>
2024-09-23 16:39:43 -0600

    create bookmark grid and bookmark card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+// Next Imports
+import Link from "next/link";
+
+// Database Imports
 import { db } from "@/db/client";
 import { bookmarks } from "@/db/schema";
+
+// Component Imports
 import { Main, Section, Container } from "@/components/craft";
 
 export default async function Home() {
@@ -11,9 +17,34 @@ export default async function Home() {
       <Section>
         <Container>
           <h1>Welcome to the Directory Template</h1>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
+
+          <BookmarkGrid>
+            {data.map((bookmark) => (
+              <BookmarkCard key={bookmark.slug} bookmark={bookmark} />
+            ))}
+          </BookmarkGrid>
         </Container>
       </Section>
     </Main>
   );
 }
+
+const BookmarkCard = ({
+  bookmark,
+}: {
+  bookmark: typeof bookmarks.$inferSelect;
+}) => {
+  return (
+    <Link href={bookmark.slug ?? "/"} className="hover:underline">
+      {bookmark.title}
+    </Link>
+  );
+};
+
+const BookmarkGrid = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {children}
+    </div>
+  );
+};
