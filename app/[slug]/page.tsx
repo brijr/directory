0bb6a38a4697f08251705b52b384878e3d1ba14f
Bridger Tower <bridgertower@gmail.com>
2024-09-24 14:50:54 -0600

    Updated

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,7 @@
 // Next Imports
 import { notFound } from "next/navigation";
 import Link from "next/link";
-
+import Image from "next/image";
 // Database Imports
 import { getBookmarkBySlug } from "@/lib/data";
 
@@ -28,7 +28,14 @@ export default async function Page({ params }: { params: { slug: string } }) {
           <div className="mb-12">
             <h1>{bookmark.title}</h1>
             <p>{bookmark.description}</p>
-            <div className="flex gap-2 my-4">
+            <Image
+              src={bookmark.screenshotUrl ?? ""}
+              width={1920}
+              height={1080}
+              alt={bookmark.title ?? ""}
+              className="rounded-md"
+            />
+            <div className="my-4 flex gap-2">
               <Button className="not-prose">
                 <Link
                   href={`${bookmark.url}?utm_source=designengineer.fyi`}
