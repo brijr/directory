c8b46fa2ae8edf562abd431de69012c33ff201e7
Bridger Tower <bridgertower@gmail.com>
2024-10-15 17:08:45 -0600

    Updated

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,7 +2,7 @@
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
-
+import Balancer from "react-wrap-balancer";
 // Database Imports
 import { getBookmarkBySlug } from "@/lib/data";
 
@@ -28,26 +28,37 @@ export default async function Page({ params }: { params: { slug: string } }) {
       <Section>
         <Container>
           <div className="mb-12">
-            <h1>{bookmark.name}</h1>
-            <p>{bookmark.description}</p>
-            <Image
-              src={bookmark.screenshot_url ?? "/placeholder.jpg"}
-              width={1920}
-              height={1080}
-              alt={bookmark.name ?? "Design Engineer Resource"}
-              className="rounded-md"
-            />
-            <div className="my-4 flex gap-2">
-              <Button className="not-prose">
-                <Link
-                  href={`${bookmark.url}?utm_source=designengineer.fyi`}
-                  target="_blank"
-                >
-                  Visit Resource
-                </Link>
-              </Button>
-              <BackButton />
+            <div className="grid grid-cols-[2fr_1fr] items-start justify-between gap-2">
+              <div className="space-y-2">
+                <h1 className="text-xl font-medium">{bookmark.name}</h1>
+                <p className="text-sm text-muted-foreground">
+                  <Balancer>{bookmark.description}</Balancer>
+                </p>
+              </div>
+              <div className="my-4 flex gap-2 justify-self-end">
+                <Button className="not-prose">
+                  <Link
+                    href={`${bookmark.url}?utm_source=designengineer.fyi`}
+                    target="_blank"
+                  >
+                    Visit Resource
+                  </Link>
+                </Button>
+                <BackButton />
+              </div>
             </div>
+            <Link
+              href={`${bookmark.url}?utm_source=designengineer.fyi`}
+              target="_blank"
+            >
+              <Image
+                src={bookmark.screenshot_url ?? "/placeholder.jpg"}
+                width={1920}
+                height={1080}
+                alt={bookmark.name ?? "Design Engineer Resource"}
+                className="my-8 rounded-md border shadow-sm"
+              />
+            </Link>
           </div>
 
           <Article>
